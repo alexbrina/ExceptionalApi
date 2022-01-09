a324d4baa2c5a2e4a3ed480869d28fec141dacb6
Alexandre Brina <alexbrina@gmail.com>
2022-01-09 08:45:44 -0300

    Exception handler global

---
+++ ExceptionalApi/ExceptionHandlerFilter.cs
@@ -0,0 +1,59 @@
+﻿using CoreDomain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+
+namespace ExceptionalApi
+{
+    public class ExceptionHandlerFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            var result = context.Exception switch
+            {
+                DomainException => DomainExceptionHandler(context),
+                _ => InternalServerErrorExceptionHandler(context)
+            };
+
+            if (result != null)
+            {
+                context.Result = result;
+                context.ExceptionHandled = true;
+            }
+        }
+
+        private static ObjectResult DomainExceptionHandler(ExceptionContext context)
+        {
+            var problemDetails = CreateBasicProblemDetails(context);
+            return new BadRequestObjectResult(problemDetails);
+        }
+
+        private static ObjectResult InternalServerErrorExceptionHandler(ExceptionContext context)
+        {
+            var problemDetails = CreateBasicProblemDetails(context);
+            problemDetails.Status = StatusCodes.Status500InternalServerError;
+            return new ObjectResult(problemDetails);
+        }
+
+        private static ProblemDetails CreateBasicProblemDetails(ExceptionContext context)
+        {
+            var problemDetails = new ProblemDetails
+            {
+                Title = context.Exception.GetType().Name,
+                Instance = context.HttpContext.Request.Path.Value,
+                Detail = context.Exception.Message
+            };
+
+            var traceId = Activity.Current?.Id
+                ?? context.HttpContext.TraceIdentifier;
+
+            if (traceId != null)
+            {
+                problemDetails.Extensions["traceId"] = traceId;
+            }
+
+            return problemDetails;
+        }
+    }
+}
--- ExceptionalApi/Startup.cs
+++ ExceptionalApi/Startup.cs
@@ -1,15 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ExceptionalApi
 {
@@ -22,18 +16,17 @@ namespace ExceptionalApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // adiciona as Controllers com ExceptionHandlerFilter global
+            services.AddControllers(options => options.Filters.Add<ExceptionHandlerFilter>());
 
-            services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ExceptionalApi", Version = "v1" });
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
